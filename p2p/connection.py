75cd5b2e256ebb5339130bbcfaca9a4f7010d4ed
Rubilmax <rmilon@gmail.com>
2020-06-24 14:50:56 +0200

    Added documentation

--- p2p/connection.py
+++ p2p/connection.py
@@ -2,46 +2,85 @@ import json
 import pickle
 import socket
 import threading
+from typing import Any
 
 from .utils import data_header, header_max_size, build_header, split_header
 
 
 class Connection():
 
-    def __init__(self, main_peer, sock: socket.socket, buffer_size: int = 2 ** 13, data_type: str = "raw"):
+    def __init__(self, main_peer, sock: socket.socket, buffer_size: int = 2 ** 13, data_type: str = "raw", strict: bool = True):
+        if self.data_type not in ["raw", "json", "bytes"]:
+            raise ValueError("data_type must be one of ['raw', 'json', 'bytes']")
+
         self.main_peer = main_peer
         self.sock = sock
         self.buffer_size = int(buffer_size)
-        self.data_type = data_type
+        self._data_type = data_type
+        self.strict = strict
 
-        self.terminate_flag = threading.Event()
+        self.stop_listener_flag = threading.Event()
         self.thread = threading.Thread(target=self._listen)
         self.thread.deamon = True
         self.thread.start()
 
-    def send(self, data):
+    @property
+    def data_type(self):
+        self._data_type
+
+    @data_type.setter
+    def data_type(self, data_type):
+        self._data_type = data_type
+
+        if self.strict:
+            # TODO: send data_type update
+            pass
+
+    def send(self, data: Any):
+        """Send data to the target peer, serializing it to this connection's default data format.
+
+        Args:
+            data (Any): the data to serialize and send
+
+        Raises:
+            ValueError: if this connection's default format is bytes and the data is not bytes
+        """
+        # first encode data according to this connection's default data type
+        if self.data_type == "raw":
+            data = pickle.dumps(data)
+        elif self.data_type == "json":
+            data = json.dumps(data)
+        else:
+            if not isinstance(data, bytes):
+                raise ValueError("data is not a bytes object")
+
+        # then send information about data
         header = build_header({
+            # so that the other peer knows how much data to handle
             "size": len(data),
+            # so that the other peer know which type of data to handle
             "data_type": self.data_type
         }, header_type=data_header)
 
         self.sock.sendall(header.encode("utf-8"))
 
-        if self.data_type == "raw":
-            data = pickle.dumps(data)
-        elif self.data_type == "json":
-            data = json.dumps(data)
-        elif self.data_type == "bytes":
-            if not isinstance(data, bytes):
-                raise ValueError("data is not a bytes object")
-        else:
-            raise ValueError("data_type must be one of ['raw', 'json', 'bytes']")
-
         self.sock.sendall(data)
 
     def _receive(self, header: str):
+        """Internal method to start the receiving process
+
+        Args:
+            header (str): the header first received from the other peer
+
+        Returns:
+            Any: the data received and deserialized (according to the data type referenced in the header)
+        """
         header = split_header(header)
 
+        if self.strict and header["data_type"] != self.data_type:
+            return None
+
+        # receiving data
         buffer = b""
         nb_chunks = header["size"] // self.buffer_size
         if header["size"] % self.buffer_size > 0:
@@ -50,6 +89,7 @@ class Connection():
         for _ in range(nb_chunks):
             buffer += self.sock.recv(self.buffer_size)
 
+        # deserializing data
         data = None
         if header["data_type"] == "raw":
             data = pickle.loads(buffer)
@@ -59,12 +99,14 @@ class Connection():
         return data
 
     def close(self):
-        self.terminate_flag.set()
+        self.stop_listener_flag.set()
 
     def _listen(self):
-        self.sock.settimeout(self.main_peer.timeout)
+        while not self.stop_listener_flag.is_set():
+            # in case timeout has changed
+            if self.main_peer.timeout != self.sock.gettimeout():
+                self.sock.settimeout(self.main_peer.timeout)
 
-        while not self.terminate_flag.is_set():
             try:
                 # will block until any header is received or socket timeout
                 header = self.sock.recv(header_max_size).decode("utf-8")
@@ -72,6 +114,7 @@ class Connection():
                 # no header received within timeout seconds
                 continue
 
+            # if we received a data header, otherwise do nothing with the received packet
             if header.startswith(data_header):
                 self.main_peer.logger.debug(f"Header received: {header}")
 
@@ -79,4 +122,5 @@ class Connection():
 
                 self.main_peer.logger.debug(f"Data received: {data}")
 
+        self.sock.close()
         self.main_peer.logger.debug("Connection stopped!")
--- p2p/peer.py
+++ p2p/peer.py
@@ -6,13 +6,15 @@ import random
 import logging
 import threading
 
+from typing import Tuple, List, Any
+
 from .utils import hello_header, header_max_size, build_header, split_header, get_local_ip, check_address
 from .connection import Connection
 
 
 class Peer():
 
-    def __init__(self, address: str = None, port: int = 0, timeout: int = 5, invisible: bool = False):
+    def __init__(self, address: str = None, port: int = 0, timeout: int = 5, invisible: bool = False, max_connections: int = 0):
         if address is None:
             address = get_local_ip()
         elif ":" in address:
@@ -23,6 +25,7 @@ class Peer():
         self.server.settimeout(timeout)
 
         self.connections = {}
+        self.max_connections = max_connections
         self.logger = logging.getLogger(f"[{self.address_name}]")
 
         self.stop_server_flag = threading.Event()
@@ -35,29 +38,78 @@ class Peer():
         self.invisible = invisible  # needs to be the last because checks the pinger_thread state
 
     @property
-    def address(self):
+    def address(self) -> Tuple[str, int]:
+        """This peer' address, in a normalized format
+
+        Returns:
+            Tuple[str, int]: the normalized address (ipv4, port)
+        """
         return self.server.getsockname()
 
     @property
-    def address_name(self):
+    def address_name(self) -> str:
+        """This peer's normalized address name
+
+        Returns:
+            str: the normalized address name ipv4:port
+        """
         return f"{self.address[0]}:{self.address[1]}"
 
     @property
-    def timeout(self):
+    def timeout(self) -> float:
+        """This peer's default timeout
+
+        Returns:
+            float: the default timeout
+        """
         return self.server.gettimeout()
 
+    @timeout.setter
+    def timeout(self, timeout: float):
+        """Sets this peer's default timeout'
+
+        Args:
+            timeout (float): the default timeout to set
+        """
+        self.server.settimeout(timeout)
+
     @property
-    def invisible(self):
+    def invisible(self) -> bool:
+        """Whether this peer is invisible to other peers on the same local network
+
+        Returns:
+            bool: this peer's invisibility
+        """
         return self._invisible
 
     @invisible.setter
-    def invisible(self, invisible):
+    def invisible(self, invisible: bool):
+        """Sets wether this peer is invisible to other peers on the same local network
+
+        Args:
+            invisible (bool): this peer's invisibility'
+        """
         self._invisible = invisible
 
+        # if this peer's now visible and the pinger thread is stopped, restart it
         if not invisible and not self.pinger_thread.is_alive():
             self.pinger_thread.start()
 
-    def connect(self, address: str, port: int = None, buffer_size: int = 2 ** 13, data_type: str = "raw") -> Connection:
+    def connect(self, address: str, port: int = None, buffer_size: int = 2 ** 13, data_type: str = "raw", strict: bool = True) -> Connection:
+        """Attempts to start a connection with a remote peer located at (address, port).
+        Additional arguments are passed to the Connection constructor and sent to the remote peer right after successful
+        connection, so that it knows with what data type to communicate with.
+
+        Args:
+            address (str): the ipv4 address of the remote peer, provided with the port if wanted (ipv4:port)
+            port (int, optional): the port to use for the connection, if not provided in address. Defaults to None.
+            buffer_size (int, optional): the buffer size to use. Defaults to 2**13.
+            data_type (str, optional): the data type to use for the connection. Defaults to "raw".
+            strict (bool, optional): whether this connection is strict on data types. Defaults to True.
+
+        Returns:
+            Connection: the connection, if established
+        """
         address, address_name = check_address(address, port)
 
         if address_name not in self.connections:
@@ -68,6 +120,7 @@ class Peer():
             sock.settimeout(self.timeout)
             sock.connect(address)
 
+            # header to send data about this peer and the data type used during this connection
             header = build_header({
                 "peer_name": self.address_name,
                 "buffer_size": buffer_size,
@@ -75,13 +128,23 @@ class Peer():
             }).encode("utf-8")
             sock.send(header)
 
-            self.connections[address_name] = Connection(self, sock, buffer_size=buffer_size, data_type=data_type)
-
+            self.connections[address_name] = Connection(
+                self,
+                sock,
+                buffer_size=buffer_size,
+                data_type=data_type,
+                strict=strict
+            )
             self.logger.debug(f"Connection established with [{address_name}]")
 
-        return self.connections[address_name]
+        return self.connections.get(address_name, None)
 
-    def get_local_peers(self):
+    def get_local_peers(self) -> List[str]:
+        """Returns the list of peers visible on the same local network.
+
+        Returns:
+            List[str]: the list of visible peers' addresses
+        """
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
@@ -109,11 +172,22 @@ class Peer():
 
         return addresses
 
-    def broadcast(self, data, data_type: str = "raw"):
+    def broadcast(self, data: Any):
+        """Broadcasts data to all the connected peers.
+
+        Args:
+            data (Any): the data to broadcast
+        """
         for connection in self.connections.values():
-            connection.send(data, data_type)
+            # TODO: handle type(data) != connection.data_type
+            connection.send(data)
 
     def stop(self, _async=False):
+        """Attempts to stop this peer and all its connections.
+
+        Args:
+            _async (bool, optional): whether to stop this peer asynchronously. Defaults to False.
+        """
         self.stop_server_flag.set()
 
         for connection in self.connections.values():
@@ -129,8 +203,10 @@ class Peer():
                 self.pinger_thread.join()
 
     def _listen_offers(self):
-        self.server.listen()
+        """Starts this peer's server, used to listen for connection requests."""
 
+        # TODO: make self.max_connections dynamic
+        self.server.listen(listen=self.max_connections)
         self.logger.info(f"Listening for connections...")
 
         while not self.stop_server_flag.is_set():
@@ -167,6 +243,8 @@ class Peer():
         self.logger.debug("Server stopped!")
 
     def _listen_pings(self):
+        """Starts this peer's pinger, used to answer pings from other seeking peers."""
+
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as pinger:
             pinger.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             pinger.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
