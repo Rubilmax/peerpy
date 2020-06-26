f922a5ffef98cf962bb86a341e053a9f05c90323
Rubilmax <rmilon@gmail.com>
2020-06-26 17:17:50 +0200

    Fixed hello world

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -5,19 +5,20 @@ sys.path.append(os.path.abspath(os.path.join('.')))  # ignore this
 from peerpy import Peer
 
 
-# handler to be executed at connection time (return value indicates whether to accept or deny the connection)
-def set_connection_handler(connection) -> bool:
-    connection.handlers["data"] = lambda data: print("Peer 2:", data)
-    return True
-
-
 # by default, Peer will find the device's local IP
 # so that it is reachable, on its default interface, by other peers on the same local network
 with Peer(port=12345, timeout=1, handlers={
-    "listen": lambda peer: print("Peer 1:", peer.address_name)
+    "listen": lambda peer: print("Peer 1:", peer.address_name)  # printing this peer's address when ready to connect
 }) as peer1:
+    # handler to be executed at connection time (returned value indicates whether to accept or deny the connection)
+    def set_connection_handler(connection) -> bool:
+        connection.handlers["data"] = lambda data: print("Peer 2:", data)
+        return connection.target_name == peer1.address_name  # only accept peer1's connections
+
     with Peer(port=11111, timeout=1, handlers={
+        # printing this peer's address when ready to connect
         "listen": lambda peer: print("Peer 2:", peer.address_name),
+        # configure connection before accepting it
         "connection": set_connection_handler
     }) as peer2:
         connection = peer1.connect(peer2.address)
